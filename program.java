fbef134d7ac831db63d142962193207d157818a8
oguzhan-solmaz <80458598+oguzhan-solmaz@users.noreply.github.com>
2021-03-11 14:28:39 +0300

    Add files via upload

---
+++ program.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+
+
+
+
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author oguzh
+ */
+public class program {
+
+    public static void main(String[] args) {
+        int a,b,c,d,i,k,j;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("ilk matrisin satir ve sutununu girin: ");
+        a=scan.nextInt();
+        b=scan.nextInt();
+        System.out.println("ikinci matrisin satir ve sutununu girin: ");
+        c=scan.nextInt();
+        d=scan.nextInt();
+        int matris1[][]=new int [a][b];
+        int matris2 [][]=new int[c][d];
+        int matris3[][]=new int[a][d];
+        if(b!=c){
+            System.out.println("matrisler carpilamaz çünkü birincinin sutun sayisi ikincinin satir sayisinden farkli");
+        }
+        else{
+            System.out.println("birinci matrisin elemanlarini girin");
+            for(i=0;i<a;i++){
+                for(j=0;j<b;j++){
+                    matris1[i][j]=scan.nextInt();
+                }
+                
+            }
+            System.out.println("ikinci matrisin elemanlarini girin");
+            for(i=0;i<c;i++){
+                for(j=0;j<d;j++){
+                    matris2[i][j]=scan.nextInt();
+                }
+            }
+            //üçüncü matris tanımlama ve çarpma işlemi
+            for(i=0;i<a;i++){
+                for(j=0;j<d;j++){
+                    for(k=0;k<b;k++){
+                        matris3[i][j]=matris3[i][j]+matris1[i][k]*matris2[k][j];
+                    }
+                }
+            }
+            for(i=0;i<a;i++){
+                for(j=0;j<d;j++){
+                    System.out.print(matris3[i][j]);
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+        
+       
+
+    }
+}
